03b6ddcb436dcd81e4022ea88b6775b90f2c6695
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-10-21 19:36:19 +0300

    patern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -34,7 +34,7 @@ class ContactForm extends Component {
             required
             onChange={this.handleChange}
             value={this.state.name}
-            pattern={"^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"}
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           />
         </label>
         <label className={css.label}>
@@ -46,7 +46,7 @@ class ContactForm extends Component {
             required
             onChange={this.handleChange}
             value={this.state.number}
-            pattern={"\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"}
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
           />
         </label>
         <button type="submit" className={css.btn}>
